99cc875765efac9945cd91dd203b994101a17c2a
tamda21 <abdetam452@gmail.com>
2022-10-15 20:30:50 +0100

    fetch data use useEffect

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,18 +1,48 @@
-import {useState} from  'react';
-import Info from './information'
+import {useState,useEffect} from  'react';
+import Info from './information';
 let Home = ()=>{
-  let [info,setInfo] = useState([
+ {/* let [info,setInfo] = useState([
     {id:1,hobbies:'programming', name:'Abde',discrpt:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse soluta ea repudiandae consequatur commodi, autem totam unde. Eos, repellat, nobis!'},
     {id:2,hobbies:'cooking', name:'Meryem',discrpt:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse soluta ea repudiandae consequatur commodi, autem totam unde. Eos, repellat, nobis!'},
     {id:3,hobbies:'Gaming', name:'Loujaine',discrpt:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse soluta ea repudiandae consequatur commodi, autem totam unde. Eos, repellat, nobis!'}
-    ]);
+    ]);*/}
+    let [info,setInfo] = useState(null);
+    let [isPending,setisPending] = useState(true);
+    let[error,setError] = useState(null)
+   {/* let handelDelet =(id)=>{
+      let newInfo =info.filter(inf=>id!==inf.id);
+      setInfo(newInfo)
+    }*/}
+    useEffect(()=>{
+      /* setTimeout just for testing your server connection*/
+      setTimeout(function() {
+fetch('http://localhost:8000/info').then(res=>{
+  if(!res.ok){
+   throw Error('Data Feiled ');
+  }
+  return res.json()
+}).then(data=>{
+        setInfo(data)
+        setisPending(false)
+        setError(null)
+      }).catch(err=>{
+        setisPending(false)
+        setError(err.message)
+      })
+    
+      }, 2000)
+      },[])
   return (
     <div className='container'>
-       <Info info={info} title="All Info!"/>
-       <Info info={info.filter(inf=>{
+    {error && <div class="red-text">{error}</div>}
+    {isPending && <div class="progress">Loading... <div class="indeterminate"></div> </div>}
+     { info && <Info info={info} title="All Info!"/>}
+       {/*<Info info={info.filter(inf=>{
          return inf.name==='Abde'||inf.name==='Meryem'
-       })} title="Info Perants !"/>
+       })} title="Info Perants !"/>*/}
+    
     </div>
     )
 }
 export default Home
+/* we use to watch your json file the command line #npx json-server --watch data/dp.json --port 8000#*/
--- src/Component/information.js
+++ src/Component/information.js
@@ -1,14 +1,15 @@
-let Info =({info,title})=>{
+let Info =({info,title,handelDelet})=>{
   return(
   <div>
   <h1 class="center red-text darken-4">{title}</h1>
    {info.map(inf=>{
          return(
-   <div class="card blue-grey darken-3 z-depth-3">
+   <div class="card blue-grey darken-3 z-depth-3" key={inf.id}>
    <div class="card-content white-text"> 
    <h3 class="card-title center">{inf.hobbies}</h3>
    <blockquote>{inf.discrpt}</blockquote>  
    <b class='right red-text'>{inf.name}</b>
+   {/*<a class="waves-effect red btn left" onClick={()=>handelDelet(inf.id)}>Delete Info</a>*/}
    </div> 
    </div>
          )
