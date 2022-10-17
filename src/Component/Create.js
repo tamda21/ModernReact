734cf49d35cc59b978bf5ea92f8ac7018734e6c8
tamda21 <abdetam452@gmail.com>
2022-10-17 18:20:26 +0100

    add data to json file

--- src/Component/Create.js
+++ src/Component/Create.js
@@ -4,14 +4,19 @@ let Create =()=>{
   let [hobbies,setHobbies] = useState("");
   let [name,setName] = useState("");
   let [discrpt,setDiscrpt] = useState("");
+  let [isPending,setIsPending ]=useState(false);
   let handelSubmit =(e)=>{
     e.preventDefault();
     let blogs ={hobbies,name,discrpt};
+    setIsPending(true)
     fetch('http://localhost:8000/info',{
       method:'POST',
       headers:{'content-type':'application/json'},
       body:JSON.stringify(blogs)
-    }).then(()=>console.log('data added '));
+    }).then(()=>{
+      console.log('data added '));
+      setIsPending(false)
+    }
   }
   return (
     <div className ='create container'>
@@ -22,10 +27,9 @@ let Create =()=>{
         <input value={name} type="text" required onChange={(e)=>{setName(e.target.value)}}/>
         
         <textarea value={discrpt} rows="8" cols="40" placeholder="Discrpt Your Self...." onChange={(e)=>{setDiscrpt(e.target. value)}}></textarea>
-        <button>Send</button>
-        <p>{hobbies}</p>
-        <p>{name}</p>
-        <p>{discrpt}</p>
+        {!isPending && <button>Send</button>}
+        {isPending && <button disabled>Send</button>}
+     
       </form>
     </div>
     )
